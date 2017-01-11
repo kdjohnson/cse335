406e6ee73312fb867b81135b655f4226c937d958
KaJuan Johnson <kdjohnson23@oakland.edu>
2017-01-11 14:48:59 -0500

    Add comments and gitignore

--- homework1.rkt
+++ homework1.rkt
@@ -1,24 +1,50 @@
 #lang racket
+#|
+Translate the following algebraic formulas into Scheme's notation.
+Type the translation into Scheme's interaction window for checking.
+((3 + 3) * 9)
+((6 * 9) / ((4 + 2) + (4 * 3)))       
+(2* ((20 - (91 / 7)) * (45 - 42)))
+|#
 (* (+ 3 3) 9)
 (/ (* 6 9) (+ (+ 4 2) (* 4 3)))
 ( * 2 (- 20 (/ 91 7)) (- 45 42))
 
+#|
+Using Scheme's define special form, write definitions of variables x, y and z,
+such that x has a value of 2 and y has a value of 3 and z has a value of 4.
+|#
 (define x 2)
 (define y 3)
 (define z 4)
 
-(define (min-335 x y z)
-  (if (and (<= x y)
-           (<= x z))
-           x
-           (if (<= y z)
-               y
-               z)))
-
-(define (sum2-largest x y z)
+#|
+Write and evaluate an if (or cond) expression in Scheme that uses
+the variables x, y and z, and that has as its value the sum of the
+two largest variables, and, if all are of the same value, the value
+of the function is 0 (zero).
+|#
+(define (sum-two-largest x y z)
   (if (and (= x y) (= y z))
       0
       (if (and (> x y) (< y z))
           (+ x z)
-          (+ y z))
-      ))
+          (+ y z)
+          )
+      )
+  )
+
+#|
+Evaluate a similar if (or cond) expression that has as its value the sum of the two
+smallest of x, y and z, if all three are of the same value, the value of the function
+is 0 (zero).
+|#
+(define (sum-two-smallest x y z)
+  (if (and (= x y) (= y z))
+      0
+      (if (and (< x y) (> y z))
+          (+ x z)
+          (+ y z)
+          )
+      )
+  )
